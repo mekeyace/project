9fe5297e857321946a24780614edebf641521dc9
mekeyace <mekeyace@nate.com>
2023-03-31 18:04:39 +0900

    master

---
+++ java1/index1.java
@@ -0,0 +1,42 @@
+package java1;	//package란 프로젝트 그룹을 말합니다.
+
+public class index1 {	//index1 class명
+	//필드공간(전역변수)
+	
+	
+	//main 메소드는 java에 기본으로 실행되는 최초의 함수
+	public static void main(String[] args) {
+		//지역변수
+		//변수선언시 무조건 자료형을 입력 받게 됩니다.
+		
+		String a = "홍길동";	//문자 자료형 변수
+		int b = 10;		//숫자 자료형 변수
+		int c = 20;
+		int d = b + c;
+		System.out.println(a);
+		System.out.println(d);
+		
+		float e = 1.5f;	//소수 자료형 변수
+		/* 
+		 float : 소수점 기준 45개 (숫자 뒤에 f)
+		 double :  소수점 기준 320개
+		 */
+		System.out.println(e);
+		
+		//숫자 자료형중 표현 가능한 범위 자료형
+		long f = 123456789123456789L;	
+		System.out.println(f);
+		/*
+		 byte : -128 ~ 127
+		 short : -32600 ~ 32600 
+		 int : -2,147,000,000 ~ 2,147,000,000
+		 long : -9,000,000,000,000,000 ~  (숫자 뒤에 L)
+		 */
+		byte aa = 127;
+		System.out.println(aa);
+		
+	}
+	
+	
+	
+}
---
+++ java1/index4.java
@@ -0,0 +1,66 @@
+package java1;
+
+public class index4 {
+
+	public static void main(String[] args) {
+		//반복문 사용(for문)
+		int f;
+		for(f=1; f<=10; f++) {
+			System.out.println(f);
+		}
+		
+		//더블for문
+		int a,b;
+		for(a=2;a<10;a++) {
+			for(b=1;b<10;b++) {
+				System.out.printf("%d*%d=%d\n",a,b,(a*b));				
+			}
+		}
+		
+		/*
+		 응용문제2.
+		 숫자 45 ~ 60까지 총 합산한 결과값을 출력하시오.
+		 */
+		int aa;
+		int sum = 0;
+		for(aa=45; aa<=60; aa++) {
+			sum += aa;
+		}
+		System.out.println(sum);
+		
+		/*
+		  응용문제3
+		  1~200까지 숫자 중 홀수값만 모두 더한 결과값을
+		  출력하시오.
+		*/
+		/*
+		 응용문제4
+		 20~60까지 숫자 중 3의 배수 값만 모두 더한 후 짝수 인지
+		 홀수 인지를 출력하시오
+		*/
+		int ff;
+		int sum2 = 0;
+		
+		for(ff=20;ff<=60;ff++) {
+			if(ff % 3 == 0) {
+				sum2 += ff;
+			}
+		}
+		
+		if(sum2 % 2 == 0) {
+			System.out.println("짝수 입니다.");
+		}
+		else {
+			System.out.println("홀수 입니다.");
+		}
+		
+		/* 
+		 응용문제5
+		 구구단 7단 ~ 3단까지 역순으로 출력되도록 하시오.
+		 단, 그 중 결과값이 홀수인 값만 출력 됩니다.		  
+		*/
+		
+		
+	}
+
+}
